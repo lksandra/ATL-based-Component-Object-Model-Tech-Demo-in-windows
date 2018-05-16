7566d24aaf15aef8e30ca53630aaa09cdc81bd7f
lsandra <lsandra@syr.edu>
2018-05-16 10:20:13 -0400

    remove linux related files and folders from windows folder

--- Project1_DO_LINUX/Client/build/Debug/GNU-Linux/main.o.d
+++
@@ -1,6 +0,0 @@
-build/Debug/GNU-Linux/main.o: main.cpp ../FileManager/IFileManager.h \
- ../FileManager/../FileManager/IUnknown.h
-
-../FileManager/IFileManager.h:
-
-../FileManager/../FileManager/IUnknown.h:
--- Project1_DO_LINUX/FileManager/build/Debug/GNU-Linux/FileManager.o.d
+++
@@ -1,8 +0,0 @@
-build/Debug/GNU-Linux/FileManager.o: FileManager.cpp FileManager.h \
- ../FileManager/IFileManager.h ../FileManager/../FileManager/IUnknown.h
-
-FileManager.h:
-
-../FileManager/IFileManager.h:
-
-../FileManager/../FileManager/IUnknown.h:
